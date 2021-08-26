af6bfd1f6a71e19278528c87a9cc1ccc16e79222
hyen <76938461+githyen@users.noreply.github.com>
2021-08-26 22:46:42 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,31 @@
-sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
-sudo apt update
-sudo apt upgrade
-sudo apt install ubuntu-restricted-extras
-sudo apt install build-essential cmake git pkg-config
-sudo apt install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev
-sudo apt install libdc1394-22-dev libxvidcore-dev libx264-dev x264
-sudo apt install libxine2-dev libv4l-dev v4l-utils
-sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt install libgtk-3-dev
-sudo apt install libatlas-base-dev libeigen3-dev gfortran
-sudo apt install python3-dev python3-numpy libtbb2 libtbb-dev
-cd ~
-mkdir opencv
-cd opencv
-wget -O opencv-3.4.0.zip https://github.com/opencv/opencv/archive/3.4.0.zip
-wget -O opencv_contrib-3.4.0.zip https://github.com/opencv/opencv_contrib/archive/3.4.0.zip
-unzip opencv-3.4.0.zip
-unzip opencv_contrib-3.4.0.zip
-mkdir build && cd build
-cmake \
--D CMAKE_BUILD_TYPE=Release \
--D CMAKE_INSTALL_PREFIX=/usr/local \
--D BUILD_WITH_DEBUG_INFO=OFF \
--D BUILD_EXAMPLES=ON \
--D BUILD_opencv_python2=OFF \
--D BUILD_opencv_python3=ON \
--D INSTALL_PYTHON_EXAMPLES=ON \
--D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.4.0/modules \
--D WITH_TBB=ON \
--D WITH_V4L=ON \
-../opencv-3.4.0/ 2>&1 | tee cmake_messages.txt
-nproc
-make -j2 2>&1 | tee build_messages.txt
-sudo make install
-sudo ldconfig
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+
+sudo apt-get install python3.5-dev python3-numpy libtbb2 libtbb-dev
+
+sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
+
+sudo -s
+cd /opt
+/opt$ git clone https://github.com/Itseez/opencv.git
+/opt$ git clone https://github.com/Itseez/opencv_contrib.git
+/opt$ cd opencv
+
+/opt/opencv$ mkdir release
+
+/opt/opencv$ cd release
+
+/opt/opencv/release$ cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
+
+/opt/opencv/release$ make -j4
+
+/opt/opencv/release$ make install
+
+/opt/opencv/release$ ldconfig
+
+/opt/opencv/release$ exit
+
+/opt/opencv/release$ cd ~
 pkg-config --modversion opencv
